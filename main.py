dfefdeee67995c98fc09d3d040986706e6cb6e88
Amir Babaei <pr.babayee@icloud.com>
2023-01-02 10:51:12 +0100

    added /archive/range & merge_and_extract_tables()
    fixed the issue with the path and created a function to be in line with DRY

--- main.py
+++ main.py
@@ -31,6 +31,17 @@ def crawl_soup(url: str) -> BeautifulSoup:
     return soup
 
 
+def merge_and_extract_tables(table_soup):
+    tables = []
+    for i in range(len(table_soup)):
+        for tr in table_soup[i].find_all("tr")[1:]:
+            table = []
+            for td in tr.find_all("td"):
+                table.append(td.text)
+            tables.append(table)
+    return tables
+
+
 @app.get("/historical/{currency}")
 @cache(expire=60 * 60 * 24)
 async def read_historical_currency(currency: str, date: str = datetime.date.today().strftime("%Y-%m")):
@@ -71,15 +82,9 @@ async def read_archive(date: str = (datetime.date.today() - datetime.timedelta(d
 
     soup = crawl_soup(BONBAST_URL + "/archive" + date.strftime("/%Y/%m/%d"))
     table_soup = soup.find_all("table")
-    tables = []
-    for i in range(len(table_soup)-1):
-        for tr in table_soup[i].find_all("tr")[1:]:
-            table = []
-            for td in tr.find_all("td"):
-                table.append(td.text)
-            tables.append(table)
+    table = merge_and_extract_tables(table_soup[:-1])
     prices = {"date": date.strftime("%Y-%m-%d")}
-    for row in tables:
+    for row in table:
         try:
             currency = row[0].lower()
             sell, buy = int(row[2]), int(row[3])
@@ -103,9 +108,9 @@ async def read_latest():
     return prices
 
 
-@app.get("/customrange/")
+@app.get("/archive/range")
 @cache(expire=60 * 60 * 24)
-async def read_archive(startDate: str = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"), endDate: str = datetime.date.today().strftime("%Y-%m-%d")):
+async def customrange(startDate: str = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"), endDate: str = datetime.date.today().strftime("%Y-%m-%d")):
     try:
         startDate = datetime.datetime.strptime(startDate, "%Y-%m-%d")
         endDate = datetime.datetime.strptime(endDate, "%Y-%m-%d")
@@ -113,32 +118,15 @@ async def read_archive(startDate: str = (datetime.date.today() - datetime.timede
         raise HTTPException(
             status_code=422, detail="Invalid Date format. Expected YYYY-MM-DD")
 
-    prices = {}
+    priceRange = {}
     duration = endDate - startDate
 
     for i in range(duration.days + 1):
         day = startDate + datetime.timedelta(days=i)
-        soup = crawl_soup(BONBAST_URL + "/archive" + day.strftime("/%Y/%m/%d"))
-        table_soup = soup.find_all("table")
-        tables = []
-        for i in range(len(table_soup)-1):
-            for tr in table_soup[i].find_all("tr")[1:]:
-                table = []
-                for td in tr.find_all("td"):
-                    table.append(td.text)
-                tables.append(table)
-        date = {}
-        for row in tables:
-            try:
-                currency = row[0].lower()
-                sell, buy = int(row[2]), int(row[3])
-                if sell > 0 and buy > 0:
-                    date[currency] = {"sell": sell, "buy": buy}
-            except ValueError:
-                pass
-        prices[day.strftime("%Y-%m-%d")] = date
-
-    return prices
+        price = await read_archive(day.strftime("%Y-%m-%d"))
+        price.pop("date")
+        priceRange[day.strftime("%Y-%m-%d")] = price
+    return priceRange
 
 
 @app.on_event("startup")
